98e1daf189920b6ca82a3c7bc3570da5aac218c4
Boolean-Operator <marktgraybill@gmail.com>
2018-02-20 15:29:19 +0000

    Version 2

---
+++ EdSherpa_v.2/app.js
@@ -0,0 +1,45 @@
+// app.js
+
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+   res.render("home"); 
+   console.log("Home Page hit");
+});
+
+app.get("/:page", function(req, res){
+   var page = req.params.page;
+   res.render(page); 
+   console.log(req.params);
+   console.log("Request for the " + page + " page has been recieved.");
+//   res.send("Navigating School Choice Process"); 
+});
+
+// app.get("/feeder", function(req, res){
+//   res.send("Finding your Feeder School Page,  Input your Address and confirm on Google Maps API"); 
+//   console.log("Request at finding feeder school page.");
+   
+// });
+
+// app.get("/school", function(req, res){
+//   res.send("Finding a school that matches resources with you students needs."); 
+//   console.log("Request at finding school process.");
+
+// });
+
+
+app.get("*", function(req, res) {
+    res.send("I'm sorry Dave, I'm afraid I can't do that.")
+});
+
+
+
+
+// Listen for PORT IP address
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Hello, Dave.  I have started the server for you ... ");
+});
